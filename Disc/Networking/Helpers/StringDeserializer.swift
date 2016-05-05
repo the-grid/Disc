a77a5d4f13de6fbb832c33e57a8cbfb17da7b7ae
Nick Velloff <nick.velloff@gmail.com>
2016-05-05 03:08:38 -0700

    cast response to NSData

--- Disc/Networking/Helpers/StringDeserializer.swift
+++ Disc/Networking/Helpers/StringDeserializer.swift
@@ -3,7 +3,7 @@ import Swish
 
 struct StringDeserializer: Deserializer {
     func deserialize<T : Request>(request: T) -> (NSData?) -> Result<T.ResponseParser.Representation, SwishError> {
-        return { (data: AnyObject?) -> Result<T.ResponseParser.Representation, SwishError> in
+        return { (data: NSData?) -> Result<T.ResponseParser.Representation, SwishError> in
             let string = self.parseString(data)
             switch string {
             case let .Failure(e):
@@ -16,10 +16,11 @@ struct StringDeserializer: Deserializer {
 }
 
 private extension StringDeserializer {
-    private func parseString(data: AnyObject?) -> Result<String, NSError> {
-        guard let d = data as? String else {
-            return .Success("")
+    private func parseString(data: NSData?) -> Result<String, NSError> {
+        guard let d = data,
+            s = String(data: d, encoding: NSUTF8StringEncoding) else {
+                return .Success("")
         }
-        return .Success(d)
+        return .Success(s)
     }
 }
